05d920e4dd55c96128e135e556327271646db45b
tim <tim_vandenbosch@msn.com>
2022-06-13 15:09:39 +0200

    feat: adjusting docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
 	title: "iTVDB",
 	tagline: "From vision to reality",
-	url: "https://itvdb.com",
+	url: "itvdb.github.io",
 	baseUrl: "/",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
@@ -18,6 +18,7 @@ const config = {
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "itvdb", // Usually your GitHub org/user name.
 	projectName: "itvdb", // Usually your repo name.
+	trailingSlash: false,
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
