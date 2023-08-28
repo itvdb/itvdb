16c1b92af4ee667e3cdaf3f085d823f3ad8f4d22
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2023-08-28 11:32:10 +0200

    Adjusting homepage with other icons

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,43 +4,93 @@ import styles from "./styles.module.css";
 
 type FeatureItem = {
 	title: string;
-	Svg: React.ComponentType<React.ComponentProps<"svg">>;
+	Svg: any;
 	description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
 	{
 		title: "Contact",
-		Svg: require("@site/static/img/agreement-deal-friends-svgrepo-com.svg").default,
-		description: <>Een gratis kennismakingsgesprek kan ten alle tijden.</>,
-	},
-	{
-		title: "Een samenwerking op maat",
-		Svg: require("@site/static/img/connection-people-share-svgrepo-com.svg").default,
+		Svg: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+				/>
+			</svg>
+		),
 		description: (
 			<>
-				De opdracht bepaald de aanpak en benadering, hier maken goede afspraken goede
-				vrienden. Ik ondersteun graag elk team met een volledige aanpak.
+				Bereik ons moeiteloos via onze online planner, e-mail en sociale media voor al uw
+				vragen.
 			</>
 		),
 	},
 	{
-		title: "Eerlijkheid",
-		Svg: require("@site/static/img/employee-for-looking-svgrepo-com.svg").default,
-		description: (
-			<>
-				Liefst dat een project tot een goed eind kan gebracht worden, als ik iets niet kan
-				zal ik mijn netwerk tot uw beschikking stellen.
-			</>
+		title: "Diensten",
+		Svg: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+				/>
+			</svg>
 		),
+		description: (
+			<>Ontdek op maat gemaakte oplossingen die aansluiten op uw behoeften en doelen.</>
+		),
+	},
+	{
+		title: "Netwerk",
+		Svg: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+				/>
+			</svg>
+		),
+		description: <>Een netwerk van freelancers zodat jouw vraag beantwoord kan worden.</>,
 	},
 ];
 
 function Feature({ title, Svg, description }: FeatureItem) {
 	return (
 		<div className={clsx("col col--4")}>
-			<div className="text--center">
-				<Svg className={styles.featureSvg} role="img" />
+			<div
+				className="text--center"
+				style={{
+					width: 80,
+					minHeight: 80,
+					marginLeft: "auto",
+					marginRight: "auto",
+				}}
+			>
+				{Svg}
 			</div>
 			<div className="text--center padding-horiz--md">
 				<h3>{title}</h3>
